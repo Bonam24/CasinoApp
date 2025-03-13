cbb49f0ae3318fce7224d01899b8b5de4c9dd8da
Bonam24 <bosene@usiu.ac.ke>
2025-03-13 11:38:29 +0300

    Just trying by adding domain

--- src/app/components/utils/supabase/supabaseClient.js
+++ src/app/components/utils/supabase/supabaseClient.js
@@ -1,5 +1,5 @@
 // src/utils/supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
+/*import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
@@ -8,4 +8,26 @@ if (!supabaseUrl || !supabaseKey) {
   throw new Error(`Supabase URL IS ${supabaseUrl} and/or API key is ${supabaseKey} are missing. Check your environment variables.`);
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);*/
+
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      cookieOptions: { 
+        domain: '.bundlesbets.casino',  // Ensure cookies work across subdomains
+        path: '/',
+        secure: true,  // Enforce HTTPS
+        sameSite: 'Lax', // Allows session sharing across subdomains
+      },
+    },
+  }
+);
+
+
